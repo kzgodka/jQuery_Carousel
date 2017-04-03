9b10d90bc47b569c39868aab6257638493523dfc
Mateusz Jabłoński <Mateusz Jaboski>
2017-04-03 19:39:13 +0200

    custom actions for slide dots

--- script/script.js
+++ script/script.js
@@ -1,59 +1,68 @@
-$(function(){
-	var carouselList = $("#carousel ul");
-	// setInterval(changeSlide, 3000);
-	function changeSlide() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+$(function () {
+    var carouselList = $("#carousel ul");
+    // setInterval(changeSlide, 3000);
+    function changeSlide(mover) {
+        carouselList.animate({'marginLeft': mover}, 500, moveFirstSlide);
+    }
 
-	};
-	function moveFirstSlide () {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
-	};
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft: 0});
+    }
 
-	var imgNum = $("#carousel img").length;
-	console.log(imgNum);
-
-
-	function dotActive () {
-		$("#dots span").bind('click', function(){
-		var index = $(this).index() + 1;
-		console.log(index);
-		$(".dot").removeClass("active");
-        $("#dot_" + index).addClass("active");
-	});	
-}
-
-	dotActive();
+    var imgNum = $("#carousel img").length;
 
 
+    function dotActive() {
+        $("#dots span").bind('click', function () {
+            var index = $(this).attr('data-dot-id');
+            var slide = 'li[data-slide-id=' + index + ']';
 
-	function moveLastSlide () {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-		firstItem.before(lastItem);
-		carouselList.css({marginLeft:-400});
-	};
+            checkPosition(index, slide);
 
-	function changeSlidePrev() {
-		moveLastSlide();
-		carouselList.animate({'marginLeft':0}, 500);
-	};
+            $(".dot").removeClass("active");
+            $("#dot_" + index).addClass("active");
+        });
+    }
 
-	var nextSlide = $(".next");
-	nextSlide.click(function () {
-		changeSlide();
-	});
+    dotActive();
 
-	var prevSlide = $(".prev");
-	prevSlide.click(function () {
-		changeSlidePrev();
-	});
+    function checkPosition(index, slide) {
+        var slides = $('ul li');
+        $.each(slides, function (i, item) {
+            if ($(item).attr('data-slide-id') === index) {
+                console.log(i);
+                var mover = -((i+1)*400);
+                changeSlide(mover);
+            }
+        });
+    }
 
-	
+    function moveLastSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        firstItem.before(lastItem);
+        carouselList.css({marginLeft: -400});
+    }
 
-	
+    function changeSlidePrev() {
+        moveLastSlide();
+        carouselList.animate({'marginLeft': 0}, 500);
+    }
+
+    var nextSlide = $(".next");
+    nextSlide.click(function () {
+        changeSlide(-400);
+    });
+
+    var prevSlide = $(".prev");
+    prevSlide.click(function () {
+        changeSlidePrev();
+    });
+
+
 });
 
 
